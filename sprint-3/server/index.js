9ae698451a3ca87b08c95bfe9631b3793ac0b065
RylanFerrer <rylanferrer@gmail.com>
2019-11-13 13:20:27 -0800

    added sprints 3 folder to project

---
+++ sprint-3/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express()
+
+
+app.get('/', (req,res) =>{
+
+});
+
+app.listen(5000, (req,res) => [
+    console.log("Server is listening on Port 5000")
+])
